9f682de5b25a5766f633acd3c99f22c6bd3cbb4e
arsen-andreichuk <70066934+arsen-andreichuk@users.noreply.github.com>
2023-10-17 11:52:35 +0300

    Add files via upload

---
+++ components/TaskCounter.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "TaskCounter",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>Counter</h1>
+  <h2>Count: {{ count }}</h2>
+  <button @click="count++">+</button>
+</template>
